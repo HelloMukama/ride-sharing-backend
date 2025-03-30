629ab846e20b1395ff382121f5105aef2c14d185
hellomukama <ntaryebwa.mukama@gmail.com>
2025-03-30 11:33:56 +0300

    configured go run main.go at base dir

--- main.go
+++ main.go
@@ -3,20 +3,12 @@ package main
 import (
     "os"
     "os/exec"
-    "path/filepath"
 )
 
 func main() {
-    // Get absolute path to src/main.go
-    srcPath := filepath.Join(".", "src", "main.go")
-    
-    // Build the command
-    cmd := exec.Command("go", "run", srcPath)
+    cmd := exec.Command("go", "run", "./src")
     cmd.Stdout = os.Stdout
     cmd.Stderr = os.Stderr
-    
-    // Run it
-    if err := cmd.Run(); err != nil {
-        os.Exit(1)
-    }
+    cmd.Stdin = os.Stdin
+    cmd.Run()
 }
